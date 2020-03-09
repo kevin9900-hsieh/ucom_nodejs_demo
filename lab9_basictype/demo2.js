60466e5242cdb7b0818c4c255ccedbde6167ffe1
kevinhsieh <kevinhsieh@ccc.com>
2020-03-09 17:31:45 +0800

    add basictype

---
+++ lab9_basictype/demo2.js
@@ -0,0 +1,9 @@
+var x = 5
+var y = 3.14
+var z = true
+var p = 'hello'
+var q = "hello"
+var t = [1, 2, 3]
+var h = null
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q)
+console.log(typeof t, typeof h)
---
+++ lab9_basictype/demo4.js
@@ -0,0 +1,6 @@
+console.log(1.5+1.2==2.7)
+console.log(512*512)
+console.log(0.4+0.2==0.6)
+console.log(1-0.4==0.6)
+console.log(1-0.5==0.5)
+console.log(1-0.3+0.2==0.9)
