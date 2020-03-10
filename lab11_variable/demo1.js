1b333413546d164aaae6bd5e517afecee9ac6f59
kevinhsieh <kevinhsieh@ccc.com>
2020-03-10 15:54:30 +0800

    add variable

---
+++ lab11_variable/demo1.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log(x2)
+//console.log(x1)
+x1 = 'hello'
+var x2 = 'world'
+console.log(typeof x1, typeof x2)
+console.log(x1, x2)
+function printMessage() {
+    console.log( 'inside printmessage',x1, x2)
+}
+printMessage()
