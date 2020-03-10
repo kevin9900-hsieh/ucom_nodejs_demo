958292e0b45e8431a995b6adff6842c32832e531
kevinhsieh <kevinhsieh@ccc.com>
2020-03-10 14:15:14 +0800

    add basictype sample

---
+++ lab9_basictype/demo15.js
@@ -0,0 +1,18 @@
+var util = require('util')
+
+console.log(123 == '123', 25.4 == '25.4', 0 == '0')
+console.log(123 === '123', 25.4 === '25.4', 0 === '0')
+var a1 = [null, undefined, 0, false, NaN, '']
+for (i = 0; i < a1.length; i++) {
+    for (j = i + 1; j < a1.length; j++) {
+        result = util.format('%s == %s? %s',a1[i], a1[j], a1[i]==a1[j])
+        console.log(result)
+        console.log('is', a1[i], '===', a1[j], '==>',a1[i] === a1[j])
+        console.log('------------')
+    }
+}
+
+var x = 123
+var y = new Number(123)
+console.log(typeof x, typeof y)
+console.log(x == y, x === y)
---
+++ lab9_basictype/demo16.js
@@ -0,0 +1,9 @@
+function echoMe(name) {
+    console.log("echo name:",name)
+}
+echoMe("Mark")
+echoMe()
+echoMe(undefined)
+echoMe(null)
+echoMe('p','q','r')
+echoMe(['p','q','r'])
